046a1eff045c36de14c1adf811072ea2447c6e44
Kamil <kam.nas21@wp.pl>
2022-03-13 13:34:55 +0100

    Make assets folder with images,svg in react, init morgan in backend with env variables, do prod start server script

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 
+if (process.env.PROJECT_MODE === 'development') {
+	app.use(morgan('dev'));
+}
+
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+app.use('/', (req, res) => {
+	res.send({
+		message: 'Hi!',
+	});
+});
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Server is listening at port : ${SERVER_PORT}`);
