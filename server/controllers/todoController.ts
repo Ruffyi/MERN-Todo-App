0deaf5c62879cf52ba2752957be145458f598fff
Kamil <kam.nas21@wp.pl>
2022-03-13 13:57:25 +0100

    Add express async handler package to project, connect todoModel with db

--- server/controllers/todoController.ts
+++ server/controllers/todoController.ts
@@ -1,7 +1,19 @@
-const getAllTodos = (req: any, res: any) => {
-	res.send({
-		message: 'HI',
-	});
-};
+import { Request, Response, NextFunction } from 'express';
+
+import Todo from '../models/todoModel/todoModel';
+import asyncHandler from 'express-async-handler';
+
+const getAllTodos = asyncHandler(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const todos = await Todo.find({});
+
+		res.status(200).send({
+			status: 'success',
+			data: {
+				todos,
+			},
+		});
+	}
+);
 
 export { getAllTodos };
