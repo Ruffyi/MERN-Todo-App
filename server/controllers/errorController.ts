1b81cf021bec7aefee763a305668d21a8396189d
Kamil <kam.nas21@wp.pl>
2022-03-13 14:10:29 +0100

    Improve error handling, better json + prod/dev views

--- server/controllers/errorController.ts
+++ server/controllers/errorController.ts
@@ -1,10 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import CustomError from './../utils/CustomError';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './../config/.env' });
 
 const NotFoundPage = (req: Request, res: Response, next: NextFunction) => {
 	const errorMessage = `Can't find ${req.originalUrl} on server`;
 
 	next(new CustomError(errorMessage, 404));
+};
+
+const sendErrorDev = (res: Response, err: CustomError) => {
+	const { message, httpStatus = 500, status = 'error', stack } = err;
+
+	res.status(httpStatus).send({
+		error: {
+			message,
+			httpStatus,
+			status,
+		},
+		stack,
+	});
+};
+
+const sendErrorProd = (res: Response, err: CustomError) => {
+	const { message, httpStatus = 500, status = 'error' } = err;
+
+	res.status(httpStatus).send({
+		message: 'Something went wrong :(',
+		status,
+	});
 };
 
 const globalErrorMiddleware = (
@@ -13,9 +38,11 @@ const globalErrorMiddleware = (
 	res: Response,
 	next: NextFunction
 ) => {
-	res.send({
-		message: 'ERROR',
-	});
+	if (process.env.PROJECT_MODE === 'development') {
+		sendErrorDev(res, err);
+	} else if (process.env.PROJECT_MODE === 'production') {
+		sendErrorProd(res, err);
+	}
 };
 
 export { NotFoundPage };
