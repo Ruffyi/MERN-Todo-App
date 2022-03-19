3cca5d4f73e06f9e718be6e14e9f35ed947bd73c
Kamil <kam.nas21@wp.pl>
2022-03-19 12:04:48 +0100

    Remove js files

--- server/routes/todoRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var todoController_1 = require("../controllers/todoController");
-var router = (0, express_1.Router)();
-router.route('/').get(todoController_1.getAllTodos).post(todoController_1.createNewTodo);
-router.route('/:id').patch(todoController_1.updateTodo)["delete"](todoController_1.deleteTodo);
-exports["default"] = router;
